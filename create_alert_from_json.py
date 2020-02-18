989a62e74521a5749b2c11219f91b213319f40a2
BluABK <bluabk@bluabk.net>
2020-02-18 15:00:39 +0100

    Added a script for creating alerts from JSON file using TheHive4py API.

--- create_alert_from_json.py
+++ create_alert_from_json.py
@@ -1,11 +1,11 @@
 import sys
 import json
-from datetime import datetime
 
 from thehive4py.api import TheHiveApi
 from thehive4py.models import Alert
 
 if __name__ == "__main__":
+    print(sys.argv)
     if len(sys.argv) != 2:
         print("Error: Requires one single JSON file as argument!")
         exit(1)
@@ -13,20 +13,11 @@ if __name__ == "__main__":
     api_key = "YaMWlH5cr25AegAhTjntDkXNLFNlO1Yx"
     hive_api = TheHiveApi('http://{}:{}'.format("192.168.136.129", 9000), api_key)
 
-    with open(sys.argv[1], "r") as f:
+    with open(sys.argv[1]) as f:
         alert_json = json.load(f)
-        print(json.dumps(f, indent=4))
-        exit(3)
+        # print(json.dumps(alert_json, indent=4))
 
-    response = hive_api.create_alert(Alert(title="Test @ {}".format(datetime.utcnow()),
-                                           severity=3,
-                                           tlp=3,
-                                           source="splunk @ {}".format(datetime.utcnow()),
-                                           sourceRef="splunkRef",
-                                           type='external',
-                                           description="Something",
-                                           tags=[]))
-    # response = hive_api.create_alert(Alert())
+    response = hive_api.create_alert(Alert(**alert_json))
     if response.status_code == 201:
         print(json.dumps(response.json(), indent=4, sort_keys=True))
         print('')
