ba7335d1d2509677482b044b76229b0e97f43fe3
BluABK <bluabk@bluabk.net>
2020-02-20 11:59:30 +0100

    Update misc in push-to-remote script.

--- push-to-remote.bat
+++ push-to-remote.bat
@@ -9,7 +9,7 @@ ssh %REMOTE_USER%@%REMOTE_HOST% "mkdir -pv  %REMOTE_RESPONDERS_DIR%/YaraWhitelis
 REM Upload the latest copy of the responder/ dir to remote.
 scp responder\* %REMOTE_USER%@%REMOTE_HOST%:%REMOTE_RESPONDERS_DIR%"/YaraWhitelistRuleCreator"
 
-REM Convert the Windows CR-LF file to UNIX friendly (Required or crashbang fails to parse!)
+REM Convert the Windows CR-LF file to UNIX friendly (Required or shebang fails to parse!)
 ssh %REMOTE_USER%@%REMOTE_HOST% "dos2unix %REMOTE_RESPONDERS_DIR%"/YaraWhitelistRuleCreator/*.py"
 
 REM Set permissions
@@ -17,3 +17,5 @@ ssh %REMOTE_USER%@%REMOTE_HOST% "chmod -v +x %REMOTE_RESPONDERS_DIR%"/YaraWhitel
 
 REM Restart cortex (and thehive, for good measure)
 ssh %REMOTE_USER%@%REMOTE_HOST% "sudo systemctl restart cortex thehive"
+
+EXIT
