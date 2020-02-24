74526db44f7dc0def2bda1823f732991a9d7fd4f
BluABK <bluabk@bluabk.net>
2020-02-24 13:55:31 +0100

    Implemented webserver in listener.api and made it append requests to a list in webserver.

--- listener/api.py
+++ listener/api.py
@@ -2,18 +2,43 @@ import json
 
 from flask import request, jsonify
 from thehive4py.api import TheHiveApi
+from werkzeug.datastructures import ImmutableMultiDict
+
 from handlers import config_handler
 
 from yara.rules import YaraWhitelistAlertRule
+import webserver
+
+
+def get_json_returnable_observables_list(observables: json):
+    dict_list = {}
+    for observable in observables:
+        print(observable)
+        dict_list[observable['id']] = observable
+
+    return dict_list
+
+
+def imd_to_dict(imd: ImmutableMultiDict):
+    dct = {}
+    for key, value in imd.items():
+        print("{}: {}".format(key, value))
+
+    return dct
 
 
 def create_yara_whitelist_rule():
     if request.method == 'POST':
         if request.form is None:
             return "ERROR: Received data was NoneType!"
+        elif not request.is_json:
+            return "ERROR: Received data was NOT JSON!\n{}".format(request.form)
 
-        rule = YaraWhitelistAlertRule(request.form['title'], description=request.form['description'])
-        js = json.loads(json.dumps(rule.get_dict()))
+        thehive_case = request.json
+        print(json.dumps(thehive_case, indent=4))
+
+        # rule = YaraWhitelistAlertRule(request.form['title'], description=request.form['description'])
+        # js = json.loads(json.dumps(rule.get_dict()))
 
         # FIXME: Make separate function
         # Get config
@@ -23,20 +48,15 @@ def create_yara_whitelist_rule():
         hive_api = TheHiveApi('http://{}:{}'.format(config["hive_server"], config["hive_port"]), config["hive_api_key"])
 
         # Retrieve Observables in a separate API call (as they're not included in responder)
-        observables_response = hive_api.get_case_observables(request.form['id'])
-        print(observables_response)
-        observables = observables_response.json()
-        print(observables)
+        observables_response = hive_api.get_case_observables(thehive_case['id'])
 
-        dict_list = {}
-        for observable in observables:
-            print(observable)
-            dict_list[observable['id']] = observable
+        # Add observables to thehive:case as its own sub-dict
+        thehive_case['observables'] = observables_response.json()
+        # print("Case with observables:\n{}".format(json.dumps(thehive_case, indent=4)))
 
+        # Send the modified thehive:case to the webserver
+        webserver.update_content(thehive_case)
 
-
-
-        return jsonify(dict_list)
-        # return js
+        return jsonify(thehive_case)
 
 
