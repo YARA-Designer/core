9f995d58b35b9002eb15bf3331a92afece8b8a24
BluABK <bluabk@bluabk.net>
2020-05-11 17:00:13 +0200

    Removed forked cortex-responder and web-frontend code from backend/core repo.

--- yara-designer-web/static/scripts/modules/exceptions.js
+++
@@ -1,7 +0,0 @@
-const NO_CONTENTS_EXCEPTION = "NoContentsException";
-
-// Exceptions
-function NoContentsException(message) {
-    this.message = message;
-    this.name = NO_CONTENTS_EXCEPTION;
-}
