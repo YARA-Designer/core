2876463f7efbdb0a1e2eb5367730db6114e0e282
BluABK <bluabk@bluabk.net>
2020-05-14 13:20:27 +0200

    Update TestYaraString.

--- tests/yara/test_yara_string.py
+++ tests/yara/test_yara_string.py
@@ -1,15 +1,7 @@
 import unittest
 
-from handlers.yara.yara_string import YaraString, YaraModifierRestrictionError, \
+from handlers.yara_handler.yara_string import YaraString, YaraModifierRestrictionError, \
     NO_CASE, WIDE, ASCII, XOR, BASE64, BASE64_WIDE, FULL_WORD, PRIVATE
-
-
-def create_obj(obj, *args, **kwargs):
-    return obj(*args, **kwargs)
-
-
-def create_ys(*args, **kwargs):
-    return YaraString(*args, **kwargs)
 
 
 class TestYaraString(unittest.TestCase):
@@ -20,7 +12,7 @@ class TestYaraString(unittest.TestCase):
         try:
             YaraString("my_string", "potato", string_type="string",
                        modifiers=[{"type": ASCII}, {"type": WIDE}, {"type": NO_CASE}])
-        except YaraModifierRestrictionError as exc:
+        except YaraModifierRestrictionError:
             self.fail("A VALID combination of YARA String modifiers raised YaraModifierRestrictionError!")
             pass
 
@@ -31,9 +23,9 @@ class TestYaraString(unittest.TestCase):
             YaraString("my_string", "potato", string_type="string",
                        modifiers=[{"type": XOR}, {"type": WIDE}, {"type": NO_CASE}])
 
-            self.fail("test_invalid_modifier_combo didn't raise YaraModifierRestrictionError! "
+            self.fail("An INVALID combination of YARA String modifiers DIDN'T RAISE YaraModifierRestrictionError! "
                       "Restrictions are likely broken.")
-        except YaraModifierRestrictionError as exc:
+        except YaraModifierRestrictionError:
             pass
 
 
