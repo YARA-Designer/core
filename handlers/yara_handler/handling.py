5a59a5cc304a263f163066ba2232b29c65a74e8c
BluABK <bluabk@bluabk.net>
2020-05-15 17:58:05 +0200

    Removed remaining (now) unused functions in YARA handlers handling.py.

--- handlers/yara_handler/handling.py
+++ handlers/yara_handler/handling.py
@@ -40,57 +40,6 @@ def determine_yara_source_filename(rule_name: str):
     return "{fname}{ext}".format(fname=sanitize_identifier(rule_name), ext=SOURCE_FILE_EXTENSION)
 
 
-def extract_yara_strings_dict(yara_observables: dict) -> dict:
-    """
-    Takes a YARA observables dict (varname: {observable, md5sum, modifiers} and returns a dict with only varname: observable modifiers.
-
-    :param yara_observables:
-    :return: dict
-    """
-    return {k: yara_observables[k]["observable"] for k in yara_observables}
-
-
-def get_referenced_strings(cond_stmt: str, yara_strings: List[YaraString]) -> List[YaraString]:
-    """
-    In YARA it is a SyntaxError to have unreferenced strings/vars,
-    so these need to be rinsed out before rule compilation.
-
-    :param cond_stmt: str
-    :param yara_strings: {}
-    :return: Returns dict of strings that are referenced in the conditional statement.
-    """
-    # Find all occurrences of words starting with $ (i.e. variable names)
-    r = re.compile(r'\$[\w]*\b\S+')
-    matched_condition_strings = r.findall(cond_stmt)
-
-    # Get rid of mismatches by making a list of items that only matches the actual strings/vars list.
-    confirmed_items = []
-    for matched_condition_identifier, yara_string in zip(matched_condition_strings, yara_strings):
-        if sanitize_identifier(matched_condition_identifier[1:]) == yara_string.identifier:
-            confirmed_items.append(yara_string)
-
-    return confirmed_items
-
-
-def match_to_dict(match: yara.Match, condition: str = None, matches: bool = None) -> dict:
-    """
-    Copies yara.Match attributes and misc over to a more malleable dict.
-
-    :param match:
-    :param condition:
-    :param matches:
-    :return:
-    """
-    strings_tuples: List[tuple] = match.strings
-    strings_dict: dict = {}
-
-    for some_int, var, value in strings_tuples:
-        strings_dict[var] = value
-
-    return {"matches": matches, "rule": match.rule, "namespace": match.namespace, "tags": match.tags,
-            "meta": match.meta, "strings": strings_dict, "condition": condition}
-
-
 def compile_from_source(yara_sources_dict: dict, error_on_warning=True, keep_compiled=False, **kwargs) -> dict:
     """
     Generates a YARA rule based on a given dict on the form of:
