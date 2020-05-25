b76989abba55799e82dc4f5d242a83db589528ff
BluABK <bluabk@bluabk.net>
2020-05-25 11:35:19 +0200

    Added exception handling to responder API and made it overwrite existing DB rule if one with the same case ID is received.

--- listener/api.py
+++ listener/api.py
@@ -1,10 +1,10 @@
 import json
 
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 from thehive4py.api import TheHiveApi
 from werkzeug.datastructures import ImmutableMultiDict
 
-from database.operations import add_row
+from database.operations import add_row, has_row, update_rule
 from handlers import config_handler
 
 from database.models import Rule
@@ -39,29 +39,41 @@ def create_yara_whitelist_rule():
         elif not request.is_json:
             log.error("ERROR: Received data was NOT JSON!\n{}".format(request.form))
             return "ERROR: Received data was NOT JSON!\n{}".format(request.form)
+        try:
+            thehive_case = request.json
+            case_id = thehive_case['id']
+            log.info("thehive_case: {}".format(json.dumps(thehive_case, indent=4)))
 
-        thehive_case = request.json
-        log.info("thehive_case: {}".format(json.dumps(thehive_case, indent=4)))
+            # rule = YaraWhitelistAlertRule(request.form['title'], description=request.form['description'])
+            # js = json.loads(json.dumps(rule.get_dict()))
 
-        # rule = YaraWhitelistAlertRule(request.form['title'], description=request.form['description'])
-        # js = json.loads(json.dumps(rule.get_dict()))
+            # Get config
+            config = config_handler.load_config()
 
-        # Get config
-        config = config_handler.load_config()
+            # Instantiate TheHive4py API
+            hive_api = TheHiveApi('http://{}:{}'.format(config["hive_server"], config["hive_port"]), config["hive_api_key"])
 
-        # Instantiate TheHive4py API
-        hive_api = TheHiveApi('http://{}:{}'.format(config["hive_server"], config["hive_port"]), config["hive_api_key"])
+            # Retrieve Observables in a separate API call (as they're not included in responder)
+            observables_response = hive_api.get_case_observables(case_id)
 
-        # Retrieve Observables in a separate API call (as they're not included in responder)
-        observables_response = hive_api.get_case_observables(thehive_case['id'])
+            # Add observables to thehive:case as its own sub-dict
+            thehive_case['observables'] = observables_response.json()
+            # log.debugs("Case with observables:\n{}".format(json.dumps(thehive_case, indent=4)))
 
-        # Add observables to thehive:case as its own sub-dict
-        thehive_case['observables'] = observables_response.json()
-        # log.debugs("Case with observables:\n{}".format(json.dumps(thehive_case, indent=4)))
+            rule = Rule(data=thehive_case)
 
-        # Store the modified thehive:case JSON to database.
-        add_row(Rule(data=thehive_case))
+            # Store the modified thehive:case JSON to database.
+            if has_row(Rule, case_id=case_id):
+                log.warning("Row/Object identified by {filter_by} already exists in DB! Overwriting existing entry.")
+                update_rule(case_id, update_attrs=rule.as_dict())
+            else:
+                add_row(rule)
 
-        return jsonify(thehive_case)
+            return jsonify(thehive_case)
+        except Exception as exc:
+            log.exception("An unexpected Exception occurred!", exc_info=exc)
+
+            return make_response(str(exc), 500)
+
 
 
