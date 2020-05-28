e2c7edc5c878ca42fd050e9d3f5fca82ad255924
BluABK <bluabk@bluabk.net>
2020-05-28 10:57:19 +0200

    Made CortexResponder more Flask-RESTX compliant.

--- apis/thehive.py
+++ apis/thehive.py
@@ -16,25 +16,20 @@ log = create_logger(__name__)
 
 @api.route('/cortex-responder')
 class CortexResponder(Resource):
+    @api.response(400, 'Request data type mismatch')
+    @api.response(200, "JSON response")
     def post(self):
         """Receives a thehive:case from a Cortex Responder in JSON format."""
-        if request.method != 'POST':
-            log.error("Received request with wrong method!\n{}".format(request.method))
-            return make_response("ERROR: Only POST method is allowed!", 405)
-
         if request.form is None:
             log.error("Received data was NoneType!")
-            return make_response("ERROR: Received data was NoneType!", 400)
+            api.abort(400, "Received data was NoneType")
         elif not request.is_json:
             log.error("Received data was NOT JSON!\n{}".format(request.form))
-            return make_response("ERROR: Received data was NOT JSON!", 400)
+            api.abort(400, "Received data was NOT JSON!")
         try:
             thehive_case = request.json
             case_id = thehive_case['id']
             log.info("thehive_case: {}".format(json.dumps(thehive_case, indent=4)))
-
-            # rule = YaraWhitelistAlertRule(request.form['title'], description=request.form['description'])
-            # js = json.loads(json.dumps(rule.get_dict()))
 
             # Instantiate TheHive4py API
             hive_api = TheHiveApi('http://{}:{}'.format(CONFIG["hive_server"], CONFIG["hive_port"]), CONFIG["hive_api_key"])
@@ -44,7 +39,6 @@ class CortexResponder(Resource):
 
             # Add observables to thehive:case as its own sub-dict
             thehive_case['observables'] = observables_response.json()
-            # log.debugs("Case with observables:\n{}".format(json.dumps(thehive_case, indent=4)))
 
             rule = Rule(data=thehive_case)
 
@@ -58,5 +52,4 @@ class CortexResponder(Resource):
             return jsonify(thehive_case)
         except Exception as exc:
             log.exception("An unexpected Exception occurred!", exc_info=exc)
-
-            return make_response(str(exc), 500)
+            api.abort(500, str(exc))
